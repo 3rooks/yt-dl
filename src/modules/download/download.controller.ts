a108ece865a2ca7966a105e48baf06ea11c51645
3rooks <Brookslynx@gmail.com>
2023-03-12 12:42:14 -0300

    🚧 fix docker-compose + down controller

--- src/modules/download/download.controller.ts
+++ src/modules/download/download.controller.ts
@@ -9,21 +9,21 @@ import {
     Res
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { DownloadService } from './download.service';
-import { CreateDownloadDto } from './dto/create-download.dto';
-import { UpdateDownloadDto } from './dto/update-download.dto';
-
 import { Response } from 'express';
 import { createWriteStream } from 'fs';
 import { mkdir } from 'fs/promises';
 import { OUTPUT_PATH } from 'src/utils/paths.resource';
+import { pipeline } from 'stream/promises';
 import ytdl, {
     chooseFormat,
     downloadFromInfo,
     filterFormats,
     getInfo
 } from 'ytdl-core';
+import { DownloadService } from './download.service';
+import { CreateDownloadDto } from './dto/create-download.dto';
 import { DownloadVideoDto } from './dto/download-video.dto';
+import { UpdateDownloadDto } from './dto/update-download.dto';
 
 @ApiTags('Download')
 @Controller('download')
@@ -41,31 +41,39 @@ export class DownloadController {
         @Body() body: DownloadVideoDto,
         @Res() res: Response
     ): Promise<void> {
-        const info = await getInfo(body.url);
-        const format = chooseFormat(info.formats, { quality: body.itag });
+        try {
+            const info = await getInfo(body.url);
 
-        await mkdir(`${OUTPUT_PATH}/${info.videoDetails.author.name}`, {
-            recursive: true
-        });
+            const exist = await this.downloadService.findOneByVideoId(
+                info.videoDetails.videoId
+            );
 
-        const file = downloadFromInfo(info, { format });
+            if (exist) return res.sendFile(exist.file);
 
-        file.pipe(
-            createWriteStream(
+            const format = chooseFormat(info.formats, { quality: body.itag });
+
+            await mkdir(`${OUTPUT_PATH}/${info.videoDetails.author.name}`, {
+                recursive: true
+            });
+
+            const file = downloadFromInfo(info);
+            const ws = createWriteStream(
                 `${OUTPUT_PATH}/${info.videoDetails.author.name}/${info.videoDetails.title}-${info.videoDetails.videoId}.${format.container}`
-            )
-        );
+            );
 
-        const results = await this.downloadService.saveDownload({
-            ...info.videoDetails,
-            file: `${OUTPUT_PATH}/${info.videoDetails.author.name}/${info.videoDetails.title}-${info.videoDetails.videoId}.${format.container}`
-        });
+            await pipeline([file, ws]);
 
-        file.on('end', () => {
+            const results = await this.downloadService.saveDownload({
+                ...info.videoDetails,
+                file: `${OUTPUT_PATH}/${info.videoDetails.author.name}/${info.videoDetails.title}-${info.videoDetails.videoId}.${format.container}`
+            });
+
             return res.sendFile(results.file, (err) => {
                 if (err) console.log(err);
             });
-        });
+        } catch (error) {
+            console.log('ERROR', error);
+        }
     }
 
     @Get('video')
