193c5c63890e84de34b69a4d1559e6b32cbb97d0
3rooks <Brookslynx@gmail.com>
2023-03-21 23:59:35 -0300

    ✅ Download Video [END]

--- src/utils/dl-fn/change-author.ts
+++ src/utils/dl-fn/change-author.ts
@@ -1,6 +1,7 @@
 import { DownloadService } from 'src/modules/download/download.service';
 import { Download } from 'src/modules/download/schema/download.schema';
 import { MoreVideoDetails } from 'ytdl-core';
+import { Exception } from '../error/exception-handler';
 import { outputPaths } from '../ytdl-paths';
 
 export const changeAuthor = async (
@@ -8,41 +9,49 @@ export const changeAuthor = async (
     videoDetails: MoreVideoDetails,
     downloadService: DownloadService
 ): Promise<Download> => {
-    if (exist) {
-        const { authorInfo } = exist;
-        const { id, name, thumbnails } = authorInfo;
+    try {
+        if (exist) {
+            const { authorInfo } = exist;
+            const { id, name, thumbnails } = authorInfo;
 
-        if (
-            id !== videoDetails.author.id ||
-            name !== videoDetails.author.name ||
-            thumbnails[0].url !== videoDetails.author.thumbnails[0].url
-        ) {
-            exist = await downloadService.updateById(exist._id, {
-                authorInfo: videoDetails.author
+            if (
+                id !== videoDetails.author.id ||
+                name !== videoDetails.author.name ||
+                thumbnails[0].url !== videoDetails.author.thumbnails[0].url
+            ) {
+                exist = await downloadService.updateById(exist._id, {
+                    authorInfo: videoDetails.author
+                });
+                await downloadImageAndText(downloadService, videoDetails);
+            }
+        } else {
+            exist = await downloadService.create({
+                authorInfo: videoDetails.author,
+                channelId: videoDetails.channelId
             });
             await downloadImageAndText(downloadService, videoDetails);
         }
-    } else {
-        exist = await downloadService.create({
-            authorInfo: videoDetails.author,
-            channelId: videoDetails.channelId
-        });
-        await downloadImageAndText(downloadService, videoDetails);
+        return exist;
+    } catch (error) {
+        throw Exception.create(error.message);
     }
-    return exist;
 };
 
 const downloadImageAndText = async (
     downloadService: DownloadService,
     videoDetails: MoreVideoDetails
 ): Promise<void> => {
-    const { outputText, outputImage } = await outputPaths(videoDetails);
+    try {
+        const { outputText, outputImage } = await outputPaths(videoDetails);
 
-    await downloadService.downloadImage(
-        videoDetails.author.thumbnails[
-            videoDetails.author.thumbnails.length - 1
-        ].url,
-        outputImage
-    );
-    await downloadService.saveInfoTxt(videoDetails, outputText);
+        await downloadService.downloadImage(
+            videoDetails.author.thumbnails[
+                videoDetails.author.thumbnails.length - 1
+            ].url,
+            outputImage
+        );
+        await downloadService.saveInfoTxt(videoDetails, outputText);
+    } catch (error) {
+        throw Exception.create(error.message);
+    }
 };
--- src/utils/dl-fn/exist-video.ts
+++ src/utils/dl-fn/exist-video.ts
@@ -2,25 +2,30 @@ import { DownloadService } from 'src/modules/download/download.service';
 import { DownloadItem } from 'src/modules/download/schema/download-items.schema';
 import { Download } from 'src/modules/download/schema/download.schema';
 import { MoreVideoDetails } from 'ytdl-core';
+import { Exception } from '../error/exception-handler';
 
 export const existVideo = async (
     exist: Download,
     videoDetails: MoreVideoDetails,
     downloadService: DownloadService
 ): Promise<DownloadItem[]> => {
-    const { videoId } = videoDetails;
-    const existVideo = exist.downloads.find((e) => e.videoId === videoId);
+    try {
+        const { videoId } = videoDetails;
+        const existVideo = exist.downloads.find((e) => e.videoId === videoId);
 
-    if (!existVideo) {
-        const output = await downloadService.downloadVideo(videoDetails);
-        exist.downloads.push({
-            filePath: output,
-            videoDetails,
-            videoId
-        });
-        await downloadService.updateById(exist._id, exist);
-        return exist.downloads;
+        if (!existVideo) {
+            const output = await downloadService.downloadVideo(videoDetails);
+            exist.downloads.push({
+                filePath: output,
+                videoDetails,
+                videoId
+            });
+            await downloadService.updateById(exist._id, exist);
+            return exist.downloads;
+        }
+
+        return [existVideo];
+    } catch (error) {
+        throw Exception.create(error.message);
     }
-
-    return [existVideo];
 };
