5bbdd2f33b7c9e8d4723a279b93b558f6b878518
3rooks <Brookslynx@gmail.com>
2023-04-11 22:03:48 -0300

    🚧 folder clean up

--- src/utils/clean-folder.ts
+++ src/utils/clean-folder.ts
@@ -1,23 +1,26 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { readdirSync } from 'fs';
-import { unlink } from 'fs/promises';
+import { rm } from 'fs/promises';
 import * as cron from 'node-cron';
 import { OUTPUT_PATH } from './paths.resource';
 
 @Injectable()
 export class CleanFolderTask {
-    private readonly folderPath = OUTPUT_PATH;
+    private readonly folder = OUTPUT_PATH;
     private readonly logger = new Logger();
 
     constructor() {
-        const cronTime = '0 */12 * * *';
+        const cronTime = '0 0 * * *';
 
         const task = cron.schedule(cronTime, async () => {
             try {
-                const files = readdirSync(this.folderPath);
+                const files = readdirSync(this.folder);
                 for (const file of files) {
                     if (file !== '.gitkeep') {
-                        await unlink(`${this.folderPath}/${file}`);
+                        await rm(`${this.folder}/${file}`, {
+                            recursive: true,
+                            force: true
+                        });
                     }
                 }
             } catch (error) {
