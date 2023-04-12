83d522a4f4c3e57493b2852ba9ad252280a86695
3rooks <Brookslynx@gmail.com>
2023-04-12 03:59:47 -0300

    🚧 error socket module

--- src/modules/download/download.module.ts
+++ src/modules/download/download.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { DownloadGatewayModule } from 'src/lib/websocket/download-gateway.module';
 import { GoogleapiModule } from '../../lib/googleyt/googleyt.module';
+import { DownloadGatewayModule } from '../../lib/websocket/download-gateway.module';
 import { CompressorModule } from '../compressor/compressor.module';
 import { YoutubeDlModule } from '../youtube-dl/youtubedl.module';
 import { DownloadController } from './download.controller';
